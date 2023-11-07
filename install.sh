d6b4c89ad2cfdb4e73304cac5d7e67511b9a704b
Mona Maret <code@monamaret.com>
2023-11-06 19:39:50 -0600

    started configs

--- install.sh
+++ install.sh
@@ -3,6 +3,14 @@
 ########
 # nvim #
 ########
+rm -rf "$HOME/.config/nvim"
 mkdir -p "$HOME/.config/nvim"
 mkdir -p "$HOME/.config/nvim/undo"
 ln -sf "$HOME/Dotfiles/nvim/init.vim" "$HOME/.config/nvim"
+
+########
+# tmux #
+########
+rm -rf "$HOME/.config/tmux"
+mkdir -p "$HOME/.config/tmux"
+ln -sf "$HOME/Dotfiles/tmux/tmux.conf" "$HOME/.config/tmux/tmux.conf"
